74b2958e1609a05087778941aa820d607928254a
Jean-Luc CANELA <jlcanelam@gmail.com>
2017-06-25 17:38:15 +0200

    adding buy a product test

--- test/unit/product.test.js
+++ test/unit/product.test.js
@@ -11,3 +11,12 @@ describe('it should resolve', function () {
             .then((res) => console.log(res));
     });
 });
+
+describe('Custom method', function () {
+    it('should allow buying a product', function () {
+        const product = new Product({ name: 'buy-product', price: 299 });
+        return product.buy(10, function (err, res) {
+            expect(res.status).to.contain('You bought 10 producit(s)');
+        });
+    });
+});
